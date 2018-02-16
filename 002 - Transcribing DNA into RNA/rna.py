c0e9f4aca98aca733df49b16d488dc3d2f7156ae
carltimms <carltimms@gmail.com>
2018-02-17 00:47:43 +1300

    Added script for Transcribing DNA into RNA.

--- 002 - Transcribing DNA into RNA/rna.py
+++ 002 - Transcribing DNA into RNA/rna.py
@@ -2,12 +2,12 @@
 
 """
 
-    'dna.py'
+    'rna.py'
 
-        rosalind.info/problems/dna
+        rosalind.info/problems/rna
 
     Carl Timms
-    16.02.2018
+    17.02.2018
 
 """
 
@@ -17,13 +17,7 @@ import sys
 try:
     with open(sys.argv[1], 'r') as f_in, open(sys.argv[1] + '-output', 'w') as f_out:
         dataset = f_in.read().rstrip()
-
-        bases = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-
-        for base in dataset:
-            bases[base] += 1
-
-        f_out.write("{} {} {} {}".format(*[bases[b] for b in "ACGT"]))
+        f_out.write(dataset.replace('T', 'U'))
 
 except Exception as e:
     print("Failed with exception:", e)
