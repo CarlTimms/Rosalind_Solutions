abc9011c30092b17b7e99ba0f15f79c2b71c9bfb
Carl Timms <carltimms@gmail.com>
2018-02-20 09:44:09 +1300

    Added solution to problem 3, Counting Point Mutations.

---
+++ 006 - Counting Point Mutations/hamm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+
+    'hamm.py'
+
+        rosalind.info/problems/hamm
+
+    Carl Timms
+    16.02.2018
+
+"""
+
+
+import sys
+
+with open(sys.argv[1], 'r') as f_in, open(sys.argv[1] + '-output.txt', 'w') as f_out:
+    data_set = f_in.read().rstrip()
+
+    hamm_dist = 0
+
+    m = (len(data_set) // 2) + 1 # Index of the first letter of the second string.
+    for i in range(m - 1):
+        if data_set[i] != data_set[i + m]:
+            hamm_dist +=1
+
+    f_out.write(str(hamm_dist))
+
+
